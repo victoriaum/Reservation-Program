f562d914821c740add03dbfde0015d8c9288344b
victoriaum <y.ilpyo95@gmail.com>
2021-11-26 21:46:48 +0900

    thymeleaf layout 설정하기

--- src/main/java/com/system/reservation/web/MainController.java
+++ src/main/java/com/system/reservation/web/MainController.java
@@ -6,16 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-/*  @Autowired
-  private InterReportService service;*/
-
   @GetMapping("/")
   public String mainPage() {
-    return "/WEB-INF/views/index.html";
+    return "index";
   }
-
-
-
 
 
 }
