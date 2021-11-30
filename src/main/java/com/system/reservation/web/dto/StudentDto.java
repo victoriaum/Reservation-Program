e270831d2a7dbd5971d5d106a4ef4ff89d3fc409
victoriaum <y.ilpyo95@gmail.com>
2021-11-30 16:34:35 +0900

    user dto 생성

---
+++ src/main/java/com/system/reservation/web/dto/StudentDto.java
@@ -0,0 +1,12 @@
+package com.system.reservation.web.dto;
+
+import lombok.Getter;
+
+@Getter
+public class StudentDto {
+  private Long id;
+  private String password;
+  private String name;
+  private String email;
+  private String grade;
+}
---
+++ src/main/java/com/system/reservation/web/dto/TeacherDto.java
@@ -0,0 +1,13 @@
+package com.system.reservation.web.dto;
+
+import lombok.Getter;
+
+@Getter
+public class TeacherDto {
+  private Long id;
+  private String password;
+  private String name;
+  private String email;
+  private String dept;
+  private String position;
+}
