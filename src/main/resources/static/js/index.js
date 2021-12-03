94e73a039fd8952ffb23a21342ab9e7f7b5af3f5
victoriaum <y.ilpyo95@gmail.com>
2021-12-04 01:25:57 +0900

    기본 index 메뉴 클릭시 이동

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,40 +1,16 @@
 $(function(){
-  $("div#result").hide();
-
-  // 로그인 클릭시
-  $("div#result").click(function(){
-    loginCheck();
+  // 메뉴 버튼 클릭시 설정
+  $("#requestMenu").click(function(){
+    location.href="/request";
   });
-
-  // 로그인 실패문구 출력
-  if("${loginuser}"==null){
-    $("div#result").html("일치하는 회원이 없습니다. 다시 로그인해주세요!");
-    $("div#result").show();
-  }
+  $("#scheduleMenu").click(function(){
+    location.href="/schedule";
+  });
+  $("#noticeMenu").click(function(){
+    location.href="/notice";
+  });
+  $("#mypageMenu").click(function(){
+    location.href="/mypage";
+  });
 });
 
-
-// 로그인 유효성 검사
-function loginCheck() {
-
-  let frm = document.loginForm;
-
-  // 아이디가 입력이 안된 경우
-  if($("input#idInput").val().trim()==null || $("input#idInput").val().trim()=="") {
-    $("div#result").show();
-    $("div#result").html("아이디를 입력해주세요!");
-    return false;
-  }
-
-  // 비밀번호가 입력이 안된 경우
-  if($("input#pwdInput").val().trim()==null || $("input#pwdInput").val().trim()=="") {
-    $("div#result").show();
-    $("div#result").html("비밀번호를 입력해주세요!");
-    return false;
-  }
-
-  frm.action="<%=ctxPath%>/login";
-  frm.method="POST";
-  frm.submit();
-
-}
