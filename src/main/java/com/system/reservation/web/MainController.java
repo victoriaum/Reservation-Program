ddca3bda566708f6b870dde52e49401765fc1437
victoriaum <y.ilpyo95@gmail.com>
2021-12-04 01:29:40 +0900

    기본 index 메뉴 클릭시 이동

--- src/main/java/com/system/reservation/web/MainController.java
+++ src/main/java/com/system/reservation/web/MainController.java
@@ -11,4 +11,24 @@ public class MainController {
     return "index";
   }
 
+  @GetMapping("/request")
+  public String request() {
+    return "request";
+  }
+
+  @GetMapping("/schedule")
+  public String schedule() {
+    return "schedule";
+  }
+
+  @GetMapping("/notice")
+  public String notice() {
+    return "notice";
+  }
+
+  @GetMapping("/mypage")
+  public String mypage() {
+    return "mypage";
+  }
+
 }
