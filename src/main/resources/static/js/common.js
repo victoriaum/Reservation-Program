f8eea8bec558753c286a1885df1ac81eccc17994
victoriaum <y.ilpyo95@gmail.com>
2021-12-04 15:13:50 +0900

    action 입력으로 사용하는 이동 메소드 추가

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,16 +1,22 @@
 $(function(){
-  // 로그아웃 클릭시
+ // 로그아웃 클릭시
   $("img#logoutBtn").click(function(){
     location.href="/login";
   });
 });
 
 
-// 보류
+// 이동할 action 기록시 이동
+function func_moveTo(action) {
+  var form = document.moveForm;
+  $("#moveForm").attr("action", action);
+  $("#moveForm").attr("method", "POST");
+  form.submit();
+}
+// 클릭시 이동
 function func_move(e) {
   var form = document.moveForm;
   $("#moveForm").attr("action", "/"+e.id);
   $("#moveForm").attr("method", "POST");
-
   form.submit();
 }
