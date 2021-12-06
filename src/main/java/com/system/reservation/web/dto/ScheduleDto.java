745334e489455ff9890b3a81e83ba991aeef6f31
victoriaum <y.ilpyo95@gmail.com>
2021-12-07 03:36:06 +0900

    타입변경

--- src/main/java/com/system/reservation/web/dto/ScheduleDto.java
+++ src/main/java/com/system/reservation/web/dto/ScheduleDto.java
@@ -1,22 +1,21 @@
 package com.system.reservation.web.dto;
 
 import com.system.reservation.domain.Schedule;
-import com.system.reservation.domain.Student;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class ScheduleDto {
-  private String schedule_no;
+  private Long schedule_no;
   private String teacher_id;
   private String student_id;
   private String schedule_date;
   private String schedule_time;
-  private String schedule_space;
+  private Integer schedule_space;
 
   @Builder
   public ScheduleDto(Schedule entity){
-    this.schedule_no = entity.getStudent_id();
+    this.schedule_no = entity.getSchedule_no();
     this.teacher_id = entity.getTeacher_id();
     this.student_id = entity.getStudent_id();
     this.schedule_date = entity.getSchedule_date();
