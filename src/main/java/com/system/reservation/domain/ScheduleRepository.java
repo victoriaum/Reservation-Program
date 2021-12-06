06a2a318ee2a7f9b04d04f5684d886dd6271c3cd
victoriaum <y.ilpyo95@gmail.com>
2021-12-07 03:16:11 +0900

    schedule repository 생성

---
+++ src/main/java/com/system/reservation/domain/ScheduleRepository.java
@@ -0,0 +1,11 @@
+package com.system.reservation.domain;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface ScheduleRepository extends JpaRepository<Student, String> {
+
+}
+
