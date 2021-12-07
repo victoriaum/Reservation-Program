f86c94bb8486406e1d88610917eebaedf9632ac8
victoriaum <y.ilpyo95@gmail.com>
2021-12-07 22:47:13 +0900

    현재날짜 시간 구하기

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,8 +1,22 @@
 $(function(){
- // 로그아웃 클릭시
+
+  // 로그아웃 클릭시
   $("img#logoutBtn").click(function(){
     location.href="/login";
   });
+
+  // 현재 날짜와 시각 구하기
+  var today = new Date();
+
+  var year = today.getFullYear();
+  var month = ('0' + (today.getMonth() + 1)).slice(-2);
+  var day = ('0' + today.getDate()).slice(-2);
+  var todayDate = year.substring(2)+month+day;
+
+  var hours = ('0' + today.getHours()).slice(-2);
+  var minutes = ('0' + today.getMinutes()).slice(-2);
+  var presentTime = hours + ':' + minutes;
+
 });
 
 
