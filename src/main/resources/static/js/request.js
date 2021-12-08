bb35cce78c07d89fccf1e1a0bb09e38f5a8a04e3
victoriaum <y.ilpyo95@gmail.com>
2021-12-08 16:09:50 +0900

    request 일정이 없는 경우, close 버튼 활성화

--- src/main/resources/static/js/request.js
+++ src/main/resources/static/js/request.js
@@ -10,7 +10,8 @@ $(function(){
 
     $(".choosenArea").append("<span class='col-4 choice checkedChoice' id='checkedDept'>"
                               +checkedDept
-                              +"<img class='closeCheckedChose' src='image/close_white.png'/>"
+                              +"<img class='closeCheckedChose' id='deptOut' "
+                                  + "onclick='func_deptOut()'src='image/close_white.png'/>"
                               +"</span>")
 
     $(".firstArea").hide();
@@ -25,8 +26,9 @@ $(function(){
         $.each(json.teacherList, function(idx, val) {
           var valArray = val.split(",");
           val = val.replace(","," ");
-          $(".secondArea").append("<span class='choice teacher' id='"+valArray[0]+","+valArray[2]+"' onclick='func_getSchedule(this.id)'>"
-                                  +valArray[0]+" "+valArray[1]+"</span>")
+          $(".secondArea").append("<span class='choice teacher' id='"+valArray[0]+","+valArray[2]+"' "
+                                  + "onclick='func_getSchedule(this.id)'>"
+                                  + valArray[0]+" "+valArray[1]+"</span>")
         });
       },
       error: function(request, status, error){
@@ -35,15 +37,17 @@ $(function(){
     });
   });
 
+
 });
 
 
 // 선생님 선택했을 때
 function func_getSchedule(id){
   var idArray = id.split(",");
-  $(".choosenArea").append("<span class='col-6 choice checkedChoice'>"
+  $(".choosenArea").append("<span class='col-6 choice checkedChoice' id='checkedTeacher'>"
                             +idArray[0]
-                            +"<img class='closeCheckedChose' src='image/close_white.png'/>"
+                            +"<img class='closeCheckedChose' id='teacherOut' "
+      + "                           onclick='func_teacherOut()' src='image/close_white.png'/>"
                             +"</span>")
 
   $(".secondArea").hide();
@@ -55,18 +59,29 @@ function func_getSchedule(id){
     dataType:"json",
     data:{checkedTeacher:idArray[1]},
     success: function(json){
-      $.each(json.scheduleList, function(idx, val) {
-        var scheduleArray = val.split(",");
-        var lastIndex = scheduleArray.length-1;
-        var subAreaDateArray = scheduleArray[1].substring(0,4)+"년 "+scheduleArray[1].substring(4,6)+"월 "+scheduleArray[1].substring(6)+"일";
-        var attenderCnt = lastIndex-4;
-        $(".thirdArea").append("<div class='subArea' id='"+scheduleArray[0]+"'>"
-                              +"<span class='date'>"+subAreaDateArray+"</span><br>"
-                              +"<span class='time'>"+scheduleArray[2]+" - "+scheduleArray[3]+"</span><br>"
-                              +"<span class='space'>"+attenderCnt+" / "+scheduleArray[lastIndex]+"</span>"
-                              +"<img class='attenderImg' src='image/request/attender.png'/>"
-                              +"</div>")
-      });
+      if(json.scheduleList==""){  // 저장된 일정이 없는 경우
+        $(".thirdArea").append("<div>정해진 일정이 없습니다.</div>");
+      }
+      else {
+        $.each(json.scheduleList, function(idx, val) {
+          var scheduleArray = val.split(",");
+          var lastIndex = scheduleArray.length - 1;
+          var subAreaDateArray = scheduleArray[1].substring(0, 4) + "년 "
+              + scheduleArray[1].substring(4, 6) + "월 "
+              + scheduleArray[1].substring(6) + "일";
+          var attenderCnt = lastIndex - 4;
+
+          $(".thirdArea").append(
+              "<div class='subArea' id='" + scheduleArray[0] + "'>"
+              + "<span class='date'>" + subAreaDateArray + "</span><br>"
+              + "<span class='time'>" + scheduleArray[2] + " - "
+              + scheduleArray[3] + "</span><br>"
+              + "<span class='space'>" + attenderCnt + " / "
+              + scheduleArray[lastIndex] + "</span>"
+              + "<img class='attenderImg' src='image/request/attender.png'/>"
+              + "</div>");
+        });
+      }
     },
     error: function(request, status, error){
       alert("code: "+request.status+"\n"+"message: "+request.responseText+"\n"+"error: "+error);
@@ -74,5 +89,31 @@ function func_getSchedule(id){
   });
 }
 
+
+// 선택한 진료과 지우기
+function func_deptOut(){
+  $("#checkedDept").remove();
+  $("#checkedTeacher").remove();
+
+  $(".firstArea").show();
+  $(".secondArea").hide();
+  $(".thirdArea").hide();
+
+  $(".secondArea").html("");
+  $(".thirdArea").html("");
+}
+
+
+// 선택한 선생님 지우기
+function func_teacherOut(){
+  $("#checkedTeacher").remove();
+
+  $(".secondArea").show();
+  $(".firstArea").hide();
+  $(".thirdArea").hide();
+
+  $(".thirdArea").html("");
+
+}
 
 
