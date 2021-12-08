0e4b4f690b15a25fa2d96057486e5b239cfc115e
victoriaum <y.ilpyo95@gmail.com>
2021-12-09 02:41:15 +0900

    .do 로 인터셉터 필요구분 제거

--- src/main/java/com/system/reservation/web/SecurityConfig.java
+++ src/main/java/com/system/reservation/web/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.system.reservation;
+package com.system.reservation.web;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
