88fcc864c5fa8a58d5ff843c23822d084e3f11a4
victoriaum <y.ilpyo95@gmail.com>
2022-01-20 13:36:30 +0900

    불필요 주석 삭제

--- src/main/java/com/system/reservation/web/SecurityConfig.java
+++ src/main/java/com/system/reservation/web/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.system.reservation.web;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
