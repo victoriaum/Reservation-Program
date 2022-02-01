3b0a27e874d94373202756073fea78f96efa29df
victoriaum <y.ilpyo95@gmail.com>
2022-02-01 14:22:46 +0900

    peopleRegister 뷰단 생성

--- src/main/resources/static/js/peopleRegister.js
+++ src/main/resources/static/js/peopleRegister.js
@@ -1,33 +1,50 @@
 $(function(){
 
-  $("#studentCheck").click();
+  func_studentBtn();
 
-  // type에 따른 필드 변경
-  $("#studentCheck").click(function(){
-    $("#teacherCheck").removeClass("clickedCheck");
-    $("#studentCheck").addClass("clickedCheck");
-    func_studentField();
-  });
-  $("#teacherCheck").click(function(){
-    $("#studentCheck").removeClass("clickedCheck");
-    $("#teacherCheck").addClass("clickedCheck");
-    func_teacherField();
-  });
+
 
 })
 
-function func_studentField(){
-  $(".helpArea").html("<span>※ 학번은 아이디와 초기 비밀번호로 입력됩니다.</span>");
-  $(".inputArea").html("<div><img class='space1 minusBtn' th:src='@{image/admin/minus.png}'/>"
-              + " <input type='text' class='space2' id='id' name='id' placeholder='학번' />"
-              + " <input type='text' class='space3' id='name' name='name' placeholder='이름' /></div>"
-              + " <div class='addBtn'><img class='/' th:src='@{image/admin/addPerson.png}'/></div>");
+
+
+// student 클릭시
+function func_studentBtn(){
+  $("#teacherCheck").removeClass("clickedCheck");
+  $("#studentCheck").addClass("clickedCheck");
+  func_studentField();
 }
 
-function func_teacherField(){
+// teacher 클릭시
+function func_teacherBtn(){
+  $("#studentCheck").removeClass("clickedCheck");
+  $("#teacherCheck").addClass("clickedCheck");
+  func_teacherField();
+}
+
+
+// student 필드 생성
+function func_studentField(){
   $(".helpArea").html("<span>※ 학번은 아이디와 초기 비밀번호로 입력됩니다.</span>");
-  $(".inputArea").html("<div><img class='space1 minusBtn' th:src='@{image/admin/minus.png}'/>"
-      + " <input type='text' class='space2' id='id' name='id' placeholder='학번' />"
-      + " <input type='text' class='space3' id='name' name='name' placeholder='이름' /></div>"
-      + " <div class='addBtn'><img class='/' th:src='@{image/admin/addPerson.png}'/></div>");
+  $(".inputArea").html("<div><img class='space1 minusBtn' src='image/admin/minus.png' onclick='func_minusStudent(this)'/>"
+              + " <input type='text' class='space2 id' name='id' placeholder='학번' />"
+              + " <input type='text' class='space3 name' name='name' placeholder='이름' /></div>");
+}
+
+// teacher 필드 생성
+function func_teacherField(){
+
+}
+
+
+// 학생 필드 입력줄 한 개 삭제
+function func_minusStudent(obj){
+  $(obj).parent().remove();
+}
+
+// 학생 필드 입력줄 한 개 추가
+function func_addStudent(obj){
+  $(".inputArea").append("<div><img class='space1 minusBtn' src='image/admin/minus.png' onclick='func_minusStudent(this)'/>"
+      + " <input type='text' class='space2 id' name='id' placeholder='학번' />"
+      + " <input type='text' class='space3 name' name='name' placeholder='이름' /></div>");
 }
