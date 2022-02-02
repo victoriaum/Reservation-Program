ed172124bf317627372624108c6c8545457f3c4d
victoriaum <y.ilpyo95@gmail.com>
2022-02-02 23:13:46 +0900

    func_peopleRegister() 생성

--- src/main/java/com/system/reservation/web/AdminController.java
+++ src/main/java/com/system/reservation/web/AdminController.java
@@ -1,12 +1,19 @@
 package com.system.reservation.web;
 
+import com.system.reservation.web.dto.StudentDto;
+import com.system.reservation.web.dto.TeacherDto;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
 @Controller
@@ -28,6 +35,11 @@ public class AdminController {
 
   @RequestMapping("/admin/peopleRegister")
   public String peopleRegister(HttpServletRequest request, Model m) {
+    return "peopleRegister";
+  }
+
+  @PostMapping("/admin/peopleRegister")
+  public String peopleRegister() {
     return "peopleRegister";
   }
 
--- src/main/resources/static/js/peopleRegister.js
+++ src/main/resources/static/js/peopleRegister.js
@@ -1,8 +1,5 @@
 $(function(){
-
   func_studentBtn();
-
-
 
 })
 
@@ -14,6 +11,7 @@ function func_studentBtn(){
   $("#studentCheck").addClass("clickedCheck");
   func_studentField();
 }
+
 
 // teacher 클릭시
 function func_teacherBtn(){
@@ -27,9 +25,10 @@ function func_teacherBtn(){
 function func_studentField(){
   $(".helpArea").html("<span>※ 학번은 아이디와 초기 비밀번호로 입력됩니다.</span>");
   $(".inputArea").html("<div><img class='space1 minusBtn' src='image/admin/minus.png' onclick='func_minusStudent(this)'/>"
-              + " <input type='text' class='space2 id' name='id' placeholder='학번' />"
-              + " <input type='text' class='space3 name' name='name' placeholder='이름' /></div>");
+              + " <input type='number' class='space2 id' placeholder='학번' />"
+              + " <input type='text' class='space3 name' placeholder='이름' /></div>");
 }
+
 
 // teacher 필드 생성
 function func_teacherField(){
@@ -42,9 +41,30 @@ function func_minusStudent(obj){
   $(obj).parent().remove();
 }
 
+
 // 학생 필드 입력줄 한 개 추가
 function func_addStudent(obj){
   $(".inputArea").append("<div><img class='space1 minusBtn' src='image/admin/minus.png' onclick='func_minusStudent(this)'/>"
-      + " <input type='text' class='space2 id' name='id' placeholder='학번' />"
-      + " <input type='text' class='space3 name' name='name' placeholder='이름' /></div>");
+      + " <input type='number' class='space2 id' placeholder='학번' />"
+      + " <input type='text' class='space3 name' placeholder='이름' /></div>");
+}
+
+
+// RegisterForm 유효성검사
+function func_peopleRegister(){
+  var form = document.peopleResiterForm;
+  var id = $(".id").val();
+  var name = $(".name").val();
+
+  if(id==null || id=="" || id=="학번" || name==null || name=="" || name=="이름"){
+    $("#error").html("빈칸이 없어야 저장이 가능합니다!");
+    return false;
+  }
+
+  var count = $(".id").length;
+
+  $("form").append("<>");
+
+  return true;
+  form.submit();
 }
